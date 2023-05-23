765bf7ad218ff8c12342c83bf7b49b5662573253
David_GG12 <garcia.iandavid117@hotmail.com>
2023-05-23 16:36:08 -0600

    lol

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
     include("connection.php");
     include("resources.php");
-
+    
     try
     {
         if(isset($_POST["login"]))
@@ -11,26 +11,28 @@
             {
                 $user_login = $_POST["user_login"];
                 $password = $_POST["password"];
-            
-                if((validate_text($user_login) == false) && (validate_text($password) == false))
+                
+                echo "lol";
+                $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
+                
+                $result = $con->query($research);
+                if($result->num_rows == 1)
                 {
-                    $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
-                        
-                    $result = $con->query($research);
-                    if($result->num_rows == 1)
-                    {
-                        header('Location: index.php');
-                        exit;
-                    }
-                    else
-                    {
-                        echo "pos no";
-                    }   
+                    header("Location: registro.html");
+                    exit;
                 }
                 else
                 {
                     echo "pos no";
+                }   
+                /*
+                if((validate_text($user_login) == false) && (validate_text($password) == false))
+                {
                 }
+                else
+                {
+                    echo "pos no";
+                }*/
             }
             else
             {

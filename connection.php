fbee424112ba27d4766dd139a8ae34fc4488c721
DavidGG12 <garcia.iandavid117@hotmail.com>
2023-05-22 21:30:15 -0600

    Programación

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 
     function connection()
     {
-        $servername = "localhost:3306";
-        $user = "root";
-        $password = "FQMC#A#NGG890";
-        $database = "locus";
+        $servername = "b6rzpd5jmxzxv6hux5yf-mysql.services.clever-cloud.com";
+        $user = "unvt0coqmwyxy2pq";
+        $password = "dAyf3jUN2wWWJg0U8xuz";
+        $database = "b6rzpd5jmxzxv6hux5yf";
         //COLOCAR UN IF PARA MANDAR A OTRA PÁGINA DE ERROR POR SI NO CONCETA CON LA BASE DE DATOS
         
         return $con = new mysqli($servername, $user, $password, $database);
--- login.php
+++ login.php
@@ -1,52 +1,36 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <title>Iniciar Sesión</title>
-        <link rel="stylesheet" href="CSS/main.css" type="text/css" />
-    </head>
-    <body>
-        <form method="post">
-            <label>Usuario:</label>
-            <br>
-            <input type="text" name="user_login" />
-            <br>
-            <br>
-            <label>Contraseña:</label>
-            <br>
-            <input type="password" name="password" />
-            <br>
-            <a href="">¿Has olvidado tu contraseña?</a>
-            <br>
-            <input type="submit" value="Iniciar Sesión" name="login">
-            <br>
-            <label>¿No tienes cuenta? </label><a href="register.php">Registrate</a>
-        </form>
-        <?php
-            include("Resources/connection.php");
-            
-            if(isset($_POST["login"]))
+<?php
+    include("connection.php");
+    try
+    {
+        if(isset($_POST["login"]))
+        {
+            $con = connection();
+            if($_SERVER["REQUEST_METHOD"] == "POST")
             {
-                $con = connection();
-
-                if($_SERVER["REQUEST_METHOD"] == "POST")
+                $user_login = $_POST["user_login"];
+                $password = $_POST["password"];
+            
+                $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
+                    
+                $result = $con->query($research);
+                if($result->num_rows == 1)
                 {
-                    $user_login = $_POST["user_login"];
-                    $password = $_POST["password"];
-                
-                    $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
-                
-                    $result = $con->query($research);
-                    if($result->num_rows == 1)
-                    {
-                        header('Location: index.php');
-                        exit;
-                    }
+                    header('Location: index.php');
+                    exit;
                 }
                 else
                 {
-                    echo "no";
+                    echo "pos no";
                 }
             }
-        ?>
-    </body>
-</html>
+            else
+            {
+                echo "no";
+            }
+        }
+    }
+    catch (ErrorException $e)
+    {
+        echo $e->getMessage();
+    }  
+?>

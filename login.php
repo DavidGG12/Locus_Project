c70ce8d59ea4348e376373e6893e666ea44aa331
DavidGG12 <garcia.iandavid117@hotmail.com>
2023-05-24 10:40:07 -0600

    Ligeros cambios para los errores

--- login.php
+++ login.php
@@ -9,38 +9,40 @@
             $con = connection();
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
-                if((validate_text($user_login) == true) && (validate_text($password) == true))
+                if((!validate_text($user_login)) && (!validate_text($password)))
                 {
-                    $user_login = $_POST["user_login"];
-                    $password = $_POST["password"];
-                    
-                    echo "lol";
-                    $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
-                    
-                    $result = $con->query($research);
-                    if($result->num_rows == 1)
-                    {
-                        header("Location: index.html");
-                        exit;
-                    }
-                    else
-                    {
-                        echo "pos no";
-                    }   
+                    echo "<script>alert('Alguno de los campos está vacío')</script>"; 
+                    header("Location: sesion.html");
+                }
+
+                $user_login = $_POST["user_login"];
+                $password = $_POST["password"];
+                
+                $research = "SELECT user_name, password_user, type_user FROM user_ WHERE user_name = '$user_login' AND password_user = '$password' ;";
+                
+                $result = $con->query($research);
+
+                if($result->num_rows != 1)
+                {
+                    echo "<script>alert('Usuario no encontrado')</script>"; 
+                    header("Location: sesion.html");
                 }
                 else
                 {
-                    echo "pos no";
-                }
+                    header("Location: index.html");
+                    exit;
+                }    
             }
             else
             {
-                echo "no";
+                echo "<script>alert('Error')</script>"; 
+                header("Location: sesion.html");
             }
         }
     }
     catch (ErrorException $e)
     {
-        echo $e->getMessage();
+        echo "<script>alert($e)</script>"; 
+        header("Location: sesion.html");
     }  
 ?>

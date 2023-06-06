0c37935966e041ff3c04e3284e5d380da7560160
Ian García <87337238+DavidGG12@users.noreply.github.com>
2023-06-06 16:37:44 -0600

    Actualizar cerrar.php

--- cerrar.php
+++ cerrar.php
@@ -2,6 +2,6 @@
 
 setcookie('user', $user, time() - 60 * 60 * 24 * 30, '/');
 
-echo '<script> alert(Sesión Cerrada) </script>';
+//echo '<script> alert(Sesión Cerrada) </script>';
 
 header("Location: index.php");
